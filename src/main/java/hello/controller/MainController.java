ef3f8f999573f6650bc0e26dee4b8b4714b815e2
18616346183 <liu3519@qq.com>
2020-05-19 22:25:53 +0800

    增加两行无用代码

--- src/main/java/hello/controller/MainController.java
+++ src/main/java/hello/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
     @RequestMapping(value = "/index")
     public String index(){
+        String str="cyttttt";
+        int num=1;
         return "index";
     }
 }
